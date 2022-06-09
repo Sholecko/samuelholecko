30e79854f798ae28b39738128eaead46ce09828d
Sholecko <107210934+Sholecko@users.noreply.github.com>
2022-06-09 21:29:05 +0200

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var input, filter, ul, li, span, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        span = li[i].getElementsByTagName("span")[0];
+        if (span.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+
+        }
+    }
+} 
